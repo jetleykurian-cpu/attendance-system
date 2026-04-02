60c72f67cc5d84e54ad1d0f59f54344051d43bc2
jetley <jetleykurian@gmail.com>
2026-04-02 13:31:05 +0530

    fix whitenoise static files storage

--- django-student-attendance-system/student_management_system/settings.py
+++ django-student-attendance-system/student_management_system/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
