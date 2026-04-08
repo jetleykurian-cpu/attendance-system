60ff70942c61b876525faaee92a058182791b5c4
jetley <jetleykurian@gmail.com>
2026-04-08 18:56:17 +0530

    add auto superser creation

---
+++ django-student-attendance-system/student_management_app/management/commands/createsu.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Create a superuser'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--username', type=str, help='Username for the superuser')
+        parser.add_argument('--email', type=str, help='Email for the superuser')
+        parser.add_argument('--password', type=str, help='Password for the superuser')
+
+    def handle(self, *args, **options):
+        username = options['username']
+        email = options['email']
+        password = options['password']
+
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(username='admin', email='admin@example.com', password='admin123')
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
